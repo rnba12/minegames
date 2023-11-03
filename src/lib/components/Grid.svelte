c234e1aa77b9efb6201c99c3c151b38378bc277d
rnba12 <rnba12@yahoo.co.uk>
2023-11-03 00:36:02 +0000

    Create game page

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let rows: number;
+    export let columns: number;
+
+
+</script>
+
+<div class="game-grid" style="--rows: {rows}; --columns: {columns}">
+    {#each Array(rows * columns) as _}
+        <div class="grid-item"><button>⬜</button></div>
+    {/each}
+</div>
+
+<style>
+    .game-grid {
+        display: grid;
+        width: fit-content;
+        grid-template-columns: repeat(var(--columns), auto);
+        grid-template-rows: repeat(var(--rows), auto);
+    }
+    .grid-item button {
+        width: 100px;
+        height: 100px;
+    }
+</style>
---
+++ src/lib/views/Game.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Grid from "../components/Grid.svelte";
+</script>
+
+<Grid rows={3} columns={3}/>
