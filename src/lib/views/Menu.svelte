5785985e8220378ec10af58c793dfc44324ed03a
rnba12 <rnba12@yahoo.co.uk>
2023-11-05 00:55:00 +0000

    Start menu form

--- src/lib/views/Menu.svelte
+++ src/lib/views/Menu.svelte
@@ -1,13 +1,28 @@
+<script lang="ts">
+    import { GameStore } from "../GameStore";
+    import { GameState } from "../types";
+
+    const handleStart = (e: Event) => {
+        GameStore.update((store) => {
+            //@ts-ignore
+            return {...store, gameState: GameState.GameStart, players: e.target?.players.value }
+        });
+    }
+</script>
+
 <div class="menu-page">
     <h1>Minegames</h1>
     <menu>
-        <button>Start</button>
-        <button>Options</button>
-        <select name="players">
-            <option value={1}>1</option>
-            <option value={1}>2</option>
-            <option value={1}>3</option>
-            <option value={1}>4</option>
-        </select>
+        <form on:submit|preventDefault={handleStart}>
+            <button on:click={handleStart}>Start</button>
+            <label for="">1</label>
+            <input type="radio" name="players" value={1}>
+            <label for="">2</label>
+            <input type="radio" name="players" value={2}>
+            <label for="">3</label>
+            <input type="radio" name="players" value={3}>
+            <label for="">4</label>
+            <input type="radio" name="players" value={4}>
+        </form>
     </menu>
 </div>
