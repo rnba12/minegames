e2f144971165729bdd2c49cca55bd60ebb5118c1
rnba12 <rnba12@yahoo.co.uk>
2023-11-12 21:55:13 +0000

    update game page

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -52,6 +52,7 @@
 
 </script>
 
+
 <div class="game-grid" style="--rows: {dimensions}; --columns: {dimensions}">
     {#each grid as tile, i}
         <div class="grid-item">
@@ -62,22 +63,48 @@
         </div>
     {/each}
 </div>
+<div class="game-info">
+    <div>Tiles Left: {numTiles}</div>
+    <div>Mines: {mines}</div>
+</div>
 
 <style>
     .game-grid {
         display: grid;
-        width: fit-content;
+        border: 1px solid black;
+        width: 450px;
+        height: 450px;
         grid-template-columns: repeat(var(--columns), auto);
         grid-template-rows: repeat(var(--rows), auto);
+        margin-bottom: 1rem;
     }
     .grid-item button {
-        width: 100px;
-        height: 100px;
+       width: 100%;
+       height: 100%;
+       border: 1px solid black;
+       border-radius: 0;
+       background: inherit;
+       padding: 0;
     }
     .selected {
-        background: blue;
+        background: blue !important;
     }
     .mine {
         background-color: red !important;
+    }
+    .game-info {
+        width: 450px;
+        font-size: 1.2rem;
+        font-weight: 600;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .game-grid {
+            width: calc(0.9 * 100vw);
+            height: calc(0.9 * 100vw);
+        }
+        .game-info {
+            width: calc(0.9 * 100vw);
+        }
     }
 </style>
