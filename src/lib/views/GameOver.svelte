392cbcac48f92ebf119a2a9bbde0a7be9a58898b
rnba12 <rnba12@yahoo.co.uk>
2023-11-12 22:12:27 +0000

    Update game over page

--- src/lib/views/GameOver.svelte
+++ src/lib/views/GameOver.svelte
@@ -7,12 +7,41 @@
     }
 </script>
 
-<div>
-    {#if $GameStore.gameState === GameState.GameOver}
-        You hit a mine!
-    {:else if $GameStore.gameState === GameState.GameWon}
-        You win!
-    {/if}
+<div class="game-over-page">
+    <div class="message">
+        {#if $GameStore.gameState === GameState.GameOver}
+            You Hit A Mine!
+        {:else if $GameStore.gameState === GameState.GameWon}
+            You Win!
+        {/if}
+    </div>
+    
+    <div class="buttons">
+        <button class="btn-dark" on:click={() => handleClick(GameState.GameStart)}>Play Again</button>
+        <button class="btn-light" on:click={() => handleClick(GameState.Menu)}>Menu</button>
+    </div>
 </div>
-<button on:click={() => handleClick(GameState.GameStart)}>Play Again</button>
-<button on:click={() => handleClick(GameState.Menu)}>Menu</button>
+
+<style>
+    .game-over-page {
+        margin-top: 8rem;
+        width: 100%;
+        text-align: center;
+    }
+    .message {
+        font-size: 2.5rem;
+        font-weight: 700;
+        margin-bottom: 1rem;
+    }
+    .buttons {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 1rem;
+    }
+    button {
+        font-size: 1.2rem;
+    }
+</style>
